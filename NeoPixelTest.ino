deb81bddd2ffab3c36e8fd4c32cb787d7dbbf880
Gideon Jones <gideon.w.jones@gmail.com>
2021-01-10 18:40:57 -0700

    Gamma Corrected color
    the colors have been corrected with gamma!

--- NeoPixelTest.ino
+++ NeoPixelTest.ino
@@ -14,6 +14,24 @@
 #define PIN      6
 #define N_LEDS 131
 
+const uint8_t PROGMEM gamma8[] = {
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
+    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
+    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
+    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
+   10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
+   17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
+   25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
+   37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
+   51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
+   69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
+   90, 92, 93, 95, 96, 98, 99,101,102,104,105,107,109,110,112,114,
+  115,117,119,120,122,124,126,127,129,131,133,135,137,138,140,142,
+  144,146,148,150,152,154,156,158,160,162,164,167,169,171,173,175,
+  177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
+  215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255 };
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(N_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
@@ -21,15 +39,15 @@ void setup() {
 }
 
 void loop() {
-  #define Blue strip.Color(pgm_read_byte(0,0,255)
-  #define Green strip.Color(pgm_read_byte(6, 108, 6)
-  #define Orange strip.Color(pgm_read_byte(255,164,8)
-  #define Purple strip.Color(75, 0, 130)
-  #define Yellow strip.Color(255, 255, 0)
-  #define Cyan strip.Color(0,255,255)//(65,181,175)
-  #define Magenta strip.Color(176, 49, 156)
-  #define Red strip.Color(255, 0, 0)
-  #define White strip.Color(255, 255, 255)
+  #define Blue strip.Color(pgm_read_byte(&gamma8[0]),pgm_read_byte(&gamma8[0]),pgm_read_byte(&gamma8[255]))
+  #define Green strip.Color(pgm_read_byte(&gamma8[6]), pgm_read_byte(&gamma8[108]), pgm_read_byte(&gamma8[6]))
+  #define Orange strip.Color(pgm_read_byte(&gamma8[255]),pgm_read_byte(&gamma8[128]),pgm_read_byte(&gamma8[0]))
+  #define Purple strip.Color(pgm_read_byte(&gamma8[75]), pgm_read_byte(&gamma8[0]), pgm_read_byte(&gamma8[130]))
+  #define Yellow strip.Color(pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[0]))
+  #define Cyan strip.Color(pgm_read_byte(&gamma8[0]),pgm_read_byte(&gamma8[255]),pgm_read_byte(&gamma8[255]))//(pgm_read_byte(&gamma8[65]),pgm_read_byte(&gamma8[181]),pgm_read_byte(&gamma8[175]))
+  #define Magenta strip.Color(pgm_read_byte(&gamma8[176]), pgm_read_byte(&gamma8[49]), pgm_read_byte(&gamma8[156]))
+  #define Red strip.Color(pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[0]), pgm_read_byte(&gamma8[0]))
+  #define White strip.Color(pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[255]), pgm_read_byte(&gamma8[255]))
 
 //  strip.setPixelColor(1, Blue);
 //  strip.setPixelColor(2, Green);
